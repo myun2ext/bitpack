77862b8e9db71afb8cde0419221f00c1b849c956
myun2 <myun2@nwhite.info>
2013-10-28 16:47:34 +0900

    bpack/def.hpp Add

---
+++ include/myun2/bpack/def.hpp
@@ -0,0 +1,11 @@
+#ifndef __github_com_myun2__bitpack__def_HPP__
+#define __github_com_myun2__bitpack__def_HPP__
+
+namespace myun2
+{
+	namespace bitpack
+	{
+	}
+}
+
+#endif//__github_com_myun2__bitpack__def_HPP__
--- include/myun2/bpack/parse.hpp
+++ include/myun2/bpack/parse.hpp
@@ -1,4 +1,11 @@
 #ifndef __github_com_myun2__bitpack__parse_HPP__
 #define __github_com_myun2__bitpack__parse_HPP__
 
+namespace myun2
+{
+	namespace bitpack
+	{
+	}
+}
+
 #endif//__github_com_myun2__bitpack__parse_HPP__
